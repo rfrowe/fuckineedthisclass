78900ef60ea8813709118104f70ad34934ba320a
Ryan <ryanf.rowe@gmail.com>
2016-04-02 01:42:15 -0700

    Added basic index and CSS for website template.

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype HTML>
+<html>
+<head>
+    <title>Fuck, I need this class.</title>
+    <link href="/css/style.css" rel="stylesheet" type="text/css" >
+</head>
+<body>
+    <header>
+        <section class="header">
+            <a id="logo" href="/">
+            <h1>Fuck, I need this class.</h1>
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li
+                    ><li><a href="/">Link 1</a></li
+                    ><li><a href="/">Link 2</a></li
+                    ><li><a href="/">Link 3</a></li
+                    ><li><a href="/">Link 4</a></li>
+                </ul>
+            </nav>
+        </section>
+    </header>
+    <section class="main">
+        <h2>Welcome</h2>
+        <p>Eiusmod elit culpa quo cillum. Duis fabulas et amet quorum, te tamen malis ipsum expetendis ad et sunt anim eram senserit, velit de ingeniis aut tamen, doctrina magna nostrud consequat, consequat hic dolor ne nam sunt nam noster ut elit incididunt nostrud. Admodum eu summis hic se sed firmissimum aut qui eram sempiternum ex fugiat illustriora pariatur tamen incurreret, legam illustriora excepteur legam nostrud, mandaremus lorem quibusdam, de ipsum quamquam nostrud, ne ipsum senserit exercitation. Nescius illum consequat do do quem cupidatat arbitrantur, ut elit cernantur admodum hic pariatur dolore consequat possumus, quid litteris ubi litteris, legam litteris ex arbitrantur, consequat quae aliquip pariatur, ea fore mandaremus. Aut velit quo aliqua. Incurreret ubi legam in qui et adipisicing id laborum amet voluptate probant ne ea id varias singulis ne minim doctrina familiaritatem ubi senserit fore commodo doctrina, fabulas fore enim vidisse magna, fugiat nescius si incurreret. Ad varias nostrud possumus.</p>
+
+        <p>Duis commodo cohaerescant, nulla quamquam singulis qui quibusdam nisi ubi mentitum philosophari nam quibusdam domesticarum ubi expetendis, incididunt ab minim quibusdam, duis vidisse hic fore labore, si amet quamquam voluptatibus, se eram excepteur consequat. Laborum quae fugiat cernantur minim. Ab ipsum coniunctione, aut mandaremus sed senserit. A quorum lorem fore aliquip, quibusdam ubi quis. Velit ullamco nam labore quorum. Admodum ad quid singulis. Legam ab laboris ea admodum exquisitaque ita expetendis.</p>
+
+        <button type="submit" id="start"><h2>Let's get started</h2></button>
+    </section>
+</body>
+</html>
